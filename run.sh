250ef09d6ab8f6eaeaa6ea3a0e6a7de8811c98a9
Arjun Suresh <arjunsuresh1987@gmail.com>
2024-10-10 23:06:29 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,11 @@
 INFERENCE_RESULTS_REPO_OWNER=${INFERENCE_RESULTS_REPO_OWNER:-mlcommons}
-INFERENCE_RESULTS_VERSION=v4.1 INFERENCE_RESULTS_REPO_OWNER=${INFERENCE_RESULTS_REPO_OWNER} INFERENCE_RESULTS_REPO_BRANCH=mlperf-inference-results-scc24 INFERENCE_RESULTS_REPO_NAME=cm4mlperf-inference bash docinit.sh
+INFERENCE_RESULTS_REPO_BRANCH=mlperf-inference-results-scc24 
+INFERENCE_RESULTS_REPO_NAME=cm4mlperf-inference
+INFERENCE_RESULTS_VERSION=v4.1
+
+if [ ! -e docs ]; then
+    git clone https://github.com/GATEOverflow/inference_results_visualization_template.git docs
+    test $? -eq 0 || exit $?
+fi
+cp docs/docinit.sh .
+bash docinit.sh
