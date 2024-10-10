39ccb9f749df36fe2c7ff7de400988d5dd2f9538
Arjun Suresh <arjunsuresh1987@gmail.com>
2024-10-10 23:18:32 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
+set -e
 export INFERENCE_RESULTS_REPO_OWNER=${INFERENCE_RESULTS_REPO_OWNER:-mlcommons}
 export INFERENCE_RESULTS_REPO_BRANCH=mlperf-inference-results-scc24 
 export INFERENCE_RESULTS_REPO_NAME=cm4mlperf-inference
 export INFERENCE_RESULTS_VERSION=v4.1
 
-if [ ! -e docs ]; then
+if [ ! -d docs ]; then
     git clone https://github.com/GATEOverflow/inference_results_visualization_template.git docs
     test $? -eq 0 || exit $?
 fi
